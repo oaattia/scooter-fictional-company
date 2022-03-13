d80edd66e88cf498dca41dba3c2aff41f256258b
Samo <osama.abdelrahman@momox.biz>
2022-03-13 15:06:25 +0100

    add user fixtures

---
+++ app/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        $user1 = new User();
+        $user1->setUuid($faker->uuid);
+        $user1->setEmail($faker->email);
+        $user1->setFirstname($faker->firstName);
+        $user1->setLastname($faker->lastName);
+
+
+        $user2 = new User();
+        $user2->setUuid($faker->uuid);
+        $user2->setEmail($faker->email);
+        $user2->setFirstname($faker->firstName);
+        $user2->setLastname($faker->lastName);
+
+
+        $manager->persist($user1);
+        $manager->persist($user2);
+
+        $manager->flush();
+    }
+}
